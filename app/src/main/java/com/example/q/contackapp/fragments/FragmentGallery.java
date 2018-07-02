f5204d208555e0632f4f7be977a41a392efed78f
sheeha97 <sheeha97@users.noreply.github.com>
2018-07-02 18:05:24 +0900

    extending gallery

--- app/src/main/java/com/example/q/contackapp/fragments/FragmentGallery.java
+++ app/src/main/java/com/example/q/contackapp/fragments/FragmentGallery.java
@@ -4,14 +4,12 @@ import android.os.Bundle;
 import android.support.annotation.Nullable;
 import android.support.v4.app.Fragment;
 import android.support.v7.widget.GridLayoutManager;
-import android.support.v7.widget.LinearLayoutManager;
 import android.support.v7.widget.RecyclerView;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 
 import com.example.q.contackapp.R;
-import com.example.q.contackapp.adapters.ContactsRvAdapter;
 import com.example.q.contackapp.adapters.RecyclerPicAdapter;
 
 public class FragmentGallery extends Fragment {
---
+++ app/src/main/java/com/example/q/contackapp/fragments/FragmentPicture.java
@@ -0,0 +1,6 @@
+package com.example.q.contackapp.fragments;
+
+import android.support.v4.app.Fragment;
+
+public class FragmentPicture extends Fragment {
+}
