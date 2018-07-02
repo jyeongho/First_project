ba9d93a06b8a98fbfc99e1777b78a5aca66ef71c
sheeha97 <sheeha97@users.noreply.github.com>
2018-07-02 10:51:26 +0900

    added two new class

---
+++ app/src/main/java/com/example/q/contackapp/fragments/FragmentCalls.java
@@ -0,0 +1,47 @@
+package com.example.q.contackapp.fragments;
+
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+import android.support.v4.app.Fragment;
+import android.support.v7.widget.LinearLayoutCompat;
+import android.support.v7.widget.LinearLayoutManager;
+import android.support.v7.widget.RecyclerView;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import com.example.q.contackapp.R;
+import com.example.q.contackapp.adapters.ContactsRvAdapter;
+import com.example.q.contackapp.models.ModelContacts;
+
+import java.util.List;
+
+public class FragmentCalls extends Fragment {
+
+    private RecyclerView recyclerView;
+    private View v;
+
+    public FragmentCalls() {
+
+    }
+
+    @Nullable
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
+        v = inflater.inflate(R.layout.frag_contacts, container, false);
+        recyclerView = v.findViewById(R.id.rv_calls);
+
+        LinearLayoutManager linearLayoutManager = new LinearLayoutManager(getContext());
+        RecyclerView.LayoutManager layoutManager = linearLayoutManager;
+        recyclerView.setLayoutManager(layoutManager);
+
+        ContactsRvAdapter adapter = new ContactsRvAdapter(getContext(), null);
+
+        return v;
+    }
+
+    private List<ModelContacts> getCallLogs() {
+
+        return null;
+    }
+}
