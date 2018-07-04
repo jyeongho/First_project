c8902175ce58816222e0ea1dd1760edd462c8500
sheeha97 <sheeha97@users.noreply.github.com>
2018-07-04 13:20:38 +0900

    part 7

--- app/src/main/java/com/example/q/contackapp/Profiles/ProfileActivity.java
+++ app/src/main/java/com/example/q/contackapp/Profiles/ProfileActivity.java
@@ -7,6 +7,7 @@ import android.support.v7.app.AppCompatActivity;
 import android.util.Log;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.support.v7.widget.Toolbar;
 
 import com.example.q.contackapp.R;
 import com.example.q.contackapp.Utils.BottomNavigationViewHelper;
@@ -22,11 +23,31 @@ public class ProfileActivity extends AppCompatActivity{
     @Override
     protected  void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_home);
+        setContentView(R.layout.activity_profile);
         Log.d(TAG, "onCreate: started");
 
         setupBottomNavigationView();
+        setupToolbar();
 
+    }
+
+    private void setupToolbar(){
+        Toolbar toolbar = (Toolbar) findViewById(R.id.profileToolBar);
+        setSupportActionBar(toolbar);
+
+        toolbar.setOnMenuItemClickListener(new Toolbar.OnMenuItemClickListener() {
+            @Override
+            public boolean onMenuItemClick(MenuItem item) {
+                Log.d(TAG, "onMenuItemClick: clicked menu item: " + item);
+
+                switch (item.getItemId()){
+                    case R.id.profileMenu:
+                        Log.d(TAG, "onMenuItemClick: Navigating to Profile Preferences.");
+                }
+
+                return false;
+            }
+        });
     }
 
     //BottomNavigationView setup
@@ -38,5 +59,11 @@ public class ProfileActivity extends AppCompatActivity{
         Menu menu = bottomNavigationViewEx.getMenu();
         MenuItem menuItem = menu.getItem(ACTIVITY_NUM);
         menuItem.setCheckable(true);
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        getMenuInflater().inflate(R.menu.profile_menu, menu);
+        return true;
     }
 }
