05f9bf014a320e8b7d89cfe08c3bbc1865a6a719
sheeha97 <sheeha97@users.noreply.github.com>
2018-07-04 16:27:34 +0900

    part 20

--- app/src/main/java/com/example/q/contackapp/Home/HomeActivity.java
+++ app/src/main/java/com/example/q/contackapp/Home/HomeActivity.java
@@ -1,6 +1,7 @@
 package com.example.q.contackapp.Home;
 
 import android.content.Context;
+import android.content.Intent;
 import android.os.Bundle;
 import android.support.annotation.NonNull;
 import android.support.design.widget.TabLayout;
@@ -10,6 +11,7 @@ import android.util.Log;
 import android.view.Menu;
 import android.view.MenuItem;
 
+import com.example.q.contackapp.Login.LoginActivity;
 import com.example.q.contackapp.R;
 import com.example.q.contackapp.Utils.BottomNavigationViewHelper;
 import com.google.firebase.auth.FirebaseAuth;
@@ -36,6 +38,46 @@ public class HomeActivity extends AppCompatActivity{
 
         setupBottomNavigationView();
         setupViewPager();
+    }
+
+    //adds 3 tabs
+    private void setupViewPager() {
+        SectionsPagerAdapter adapter = new SectionsPagerAdapter(getSupportFragmentManager());
+        adapter.addFragment(new CameraFragment()); //0
+        adapter.addFragment(new HomeFragment()); //1
+        adapter.addFragment(new MessagesFragment()); //2
+
+        ViewPager viewPager = (ViewPager) findViewById(R.id.container);
+        viewPager.setAdapter(adapter);
+
+        TabLayout tabLayout = (TabLayout) findViewById(R.id.tabs);
+        tabLayout.setupWithViewPager(viewPager);
+
+        tabLayout.getTabAt(0).setIcon(R.drawable.ic_camera);
+        tabLayout.getTabAt(1).setIcon(R.drawable.instagram);
+        tabLayout.getTabAt(2).setIcon(R.drawable.ic_arrow);
+    }
+
+    //BottomNavigationView setup
+    private void setupBottomNavigationView() {
+        Log.d(TAG, "setupBottomNavigationView: setting up BottomNavigationView");
+        BottomNavigationViewEx bottomNavigationViewEx = findViewById(R.id.bottomNavViewBar);
+        BottomNavigationViewHelper.setupBottomNavigationView(bottomNavigationViewEx);
+        BottomNavigationViewHelper.enableNavigation(mContext, bottomNavigationViewEx);
+        Menu menu = bottomNavigationViewEx.getMenu();
+        MenuItem menuItem = menu.getItem(ACTIVITY_NUM);
+        menuItem.setCheckable(true);
+    }
+
+    private void checkCurrentUser(FirebaseUser user) {
+        Log.d(TAG, "checkCurrentUser: checkign if user is logged in.");
+        //check user
+        checkCurrentUser(user);
+
+        if (user == null) {
+            Intent intent = new Intent(mContext, LoginActivity.class);
+            startActivity(intent);
+        }
     }
 
     private void setupFirebaseAuth() {
@@ -65,35 +107,5 @@ public class HomeActivity extends AppCompatActivity{
         if (mAuthListener != null) {
             mAuth.removeAuthStateListener(mAuthListener);
         }
-    }
-
-
-    //adds 3 tabs
-    private void setupViewPager() {
-        SectionsPagerAdapter adapter = new SectionsPagerAdapter(getSupportFragmentManager());
-        adapter.addFragment(new CameraFragment()); //0
-        adapter.addFragment(new HomeFragment()); //1
-        adapter.addFragment(new MessagesFragment()); //2
-
-        ViewPager viewPager = (ViewPager) findViewById(R.id.container);
-        viewPager.setAdapter(adapter);
-
-        TabLayout tabLayout = (TabLayout) findViewById(R.id.tabs);
-        tabLayout.setupWithViewPager(viewPager);
-
-        tabLayout.getTabAt(0).setIcon(R.drawable.ic_camera);
-        tabLayout.getTabAt(1).setIcon(R.drawable.instagram);
-        tabLayout.getTabAt(2).setIcon(R.drawable.ic_arrow);
-    }
-
-    //BottomNavigationView setup
-    private void setupBottomNavigationView() {
-        Log.d(TAG, "setupBottomNavigationView: setting up BottomNavigationView");
-        BottomNavigationViewEx bottomNavigationViewEx = findViewById(R.id.bottomNavViewBar);
-        BottomNavigationViewHelper.setupBottomNavigationView(bottomNavigationViewEx);
-        BottomNavigationViewHelper.enableNavigation(mContext, bottomNavigationViewEx);
-        Menu menu = bottomNavigationViewEx.getMenu();
-        MenuItem menuItem = menu.getItem(ACTIVITY_NUM);
-        menuItem.setCheckable(true);
     }
 }
